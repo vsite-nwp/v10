e269910d53b832436a08723dd3cbf247709609cd
skulenov <skulenov@gmail.com>
2016-01-17 13:44:40 +0100

    Done.

--- RecView.cpp
+++ RecView.cpp
@@ -44,6 +44,9 @@ void RecView::DoDataExchange(CDataExchange* pDX)
 {
 	CRecordView::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(RecView)
+	DDX_FieldCheck(pDX, IDC_CHECK1, m_pSet->m_manager, m_pSet);
+	DDX_FieldText(pDX, IDC_EDIT1, m_pSet->m_id, m_pSet);
+	DDX_FieldText(pDX, IDC_EDIT2, m_pSet->m_name, m_pSet);
 	//}}AFX_DATA_MAP
 }
 
@@ -110,3 +113,42 @@ CRecordset* RecView::OnGetRecordset()
 /////////////////////////////////////////////////////////////////////////////
 // RecView message handlers
 
+
+
+void RecView::OnPrint(CDC* pDC, CPrintInfo* pInfo)
+{
+	// TODO: Add your specialized code here and/or call the base class
+
+	int h_res = pDC->GetDeviceCaps(HORZRES);
+	int v_res = pDC->GetDeviceCaps(VERTRES);
+
+	CSize s_size = pDC->GetTextExtent("T");
+	int y_pos = s_size.cy;
+
+	pDC->TextOut(h_res / 10, y_pos, _T("id"));
+	pDC->TextOut(3*h_res / 10, y_pos, _T("Name"));
+	pDC->TextOut(5*h_res / 10, y_pos, _T("Manager"));
+
+	y_pos = 2.5*y_pos;
+
+	pDC->MoveTo(h_res / 10, y_pos);
+	pDC->LineTo(7 * h_res / 10, y_pos);
+
+
+	Set rs;
+	rs.Open();
+	while (!rs.IsEOF()) {
+		// draw current record
+		CString str;
+		str.Format(_T("%d"), rs.m_id);
+		y_pos +=s_size.cy;
+
+		pDC->TextOut(h_res / 10, y_pos,str);
+		pDC->TextOut(3 * h_res / 10, y_pos, rs.m_name);
+		if (rs.m_manager)
+			pDC->TextOut(5 * h_res / 10, y_pos,_T("X"));
+
+		rs.MoveNext();
+	}
+	CRecordView::OnPrint(pDC, pInfo);
+}
