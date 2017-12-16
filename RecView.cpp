c2610b43a75b1efba161b3d863527bfe951d38a8
Matija Horvat <horvat2112@gmail.com>
2017-12-16 15:42:02 +0100

    uspjesno iscrtavanja

--- RecView.cpp
+++ RecView.cpp
@@ -119,26 +119,26 @@ void RecView::OnPrint(CDC* pDC, CPrintInfo* pInfo)
 {
 	Set rs;
 	rs.Open();
-	POINT margins{100, 50};
+	POINT margins{400, 300};
 	long cellX = pDC->GetDeviceCaps(HORZRES) / rs.GetODBCFieldCount();
 	CSize strSize = pDC->GetTextExtent(std::to_string(rs.m_id).c_str());
-	RECT cell{ margins.x, margins.y,cellX, strSize.cy*2};
+	RECT cell{ margins.x, margins.y,cellX, strSize.cy+margins.y};
 	//header drawing
 	pDC->DrawText(_T("ID"), &cell, DT_LEFT|DT_BOTTOM);
-	cell = {cellX, margins.y, cellX*2, strSize.cy*2};
+	cell = {cellX, margins.y, cellX*2, strSize.cy+margins.y};
 	pDC->DrawText(_T("Name"), &cell, DT_LEFT | DT_BOTTOM);
-	cell = { cellX*2, margins.y, cellX*3, strSize.cy * 2 };
+	cell = { cellX*2, margins.y, cellX*3, strSize.cy+margins.y };
 	pDC->DrawText(_T("Manager"), &cell, DT_LEFT | DT_BOTTOM);
-	pDC->MoveTo(margins.x, strSize.cy*2);
-	pDC->LineTo(pDC->GetDeviceCaps(HORZRES) - margins.x, strSize.cy*2);
+	pDC->MoveTo(margins.x, strSize.cy+margins.y);
+	pDC->LineTo(pDC->GetDeviceCaps(HORZRES) - margins.x, strSize.cy+margins.y);
 
 	for (int movY = 2; !rs.IsEOF();movY+=2) {
-		cell = { margins.x, strSize.cy * movY+10, cellX, strSize.cy * (movY+2)+10 };
+		cell = { margins.x, strSize.cy * movY+margins.y, cellX, strSize.cy * (movY+2)+margins.y };
 		pDC->DrawText(std::to_string(rs.m_id).c_str(), &cell, DT_LEFT | DT_BOTTOM);
-		cell = { cellX, strSize.cy * movY+10, cellX * 2, strSize.cy * (movY+2)+10};
+		cell = { cellX, strSize.cy * movY+margins.y, cellX * 2, strSize.cy * (movY+2)+margins.y};
 		pDC->DrawText(rs.m_name, &cell, DT_LEFT | DT_BOTTOM);
 		if (rs.m_manager) {
-			cell = { cellX * 2, strSize.cy * movY+10, cellX * 3, strSize.cy * (movY+2)+10 };
+			cell = { cellX * 2, strSize.cy * movY+margins.y, cellX * 3, strSize.cy * (movY+2)+margins.y };
 			pDC->DrawText(_T("x"), &cell, DT_LEFT | DT_BOTTOM);
 		}
 
