4f84c2224d503688057b0de877795ef54bb13017
banov.josipa <banov.josipa@gmail.com>
2020-01-20 21:42:01 +0100

    Added database.

--- RecView.cpp
+++ RecView.cpp
@@ -43,6 +43,9 @@ RecView::~RecView()
 void RecView::DoDataExchange(CDataExchange* pDX)
 {
 	CRecordView::DoDataExchange(pDX);
+	DDX_FieldText(pDX, IDC_EDIT1, m_pSet->m_id, m_pSet);
+	DDX_FieldText(pDX, IDC_EDIT2, m_pSet->m_name, m_pSet);
+	DDX_FieldCheck(pDX, IDC_CHECK1, m_pSet->m_manager, m_pSet);
 	//{{AFX_DATA_MAP(RecView)
 	//}}AFX_DATA_MAP
 }
@@ -110,3 +113,40 @@ CRecordset* RecView::OnGetRecordset()
 /////////////////////////////////////////////////////////////////////////////
 // RecView message handlers
 
+
+
+void RecView::OnPrint(CDC* pDC, CPrintInfo* pInfo)
+{
+	int height = pDC->GetTextExtent("X").cy;
+
+	int x = pDC->GetDeviceCaps(HORZRES) / 10;
+	int y = pDC->GetDeviceCaps(VERTRES) / 20;
+
+	int userID = x + x / 2;
+	int userName = x * 3;
+	int isManager = x * 7;
+
+	pDC->TextOut(userID, y, "ID");
+	pDC->TextOut(userName, y, "name");
+	pDC->TextOut(isManager, y, "manager");
+	y += height;
+
+	pDC->MoveTo(x, y);
+	pDC->LineTo(x * 9, y);
+	y += height;
+
+	Set rs;
+	rs.Open();
+	while (!rs.IsEOF()) {
+		CString _id;
+		_id.Format(_T("%d"), rs.m_id);
+
+		pDC->TextOut(userID, y, _id);
+		pDC->TextOut(userName, y, rs.m_name);
+		if (rs.m_manager)
+			pDC->TextOut(isManager, y, "x");
+		y += height;
+
+		rs.MoveNext();
+	}
+}
