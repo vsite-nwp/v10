513dbbe0f6d8d1ca674405c600ee0006d02619c1
Dona Pejnović <don@DESKTOP-QROSDFJ>
2020-01-20 17:50:17 +0100

    to je to

--- RecView.cpp
+++ RecView.cpp
@@ -43,6 +43,10 @@ RecView::~RecView()
 void RecView::DoDataExchange(CDataExchange* pDX)
 {
 	CRecordView::DoDataExchange(pDX);
+	DDX_FieldText(pDX, IDC_EDIT1, m_pSet->m_id,m_pSet);
+	DDX_FieldText(pDX, IDC_EDIT2, m_pSet->m_name, m_pSet);
+	DDX_FieldCheck(pDX, IDC_CHECK1, m_pSet->m_manager, m_pSet);
+
 	//{{AFX_DATA_MAP(RecView)
 	//}}AFX_DATA_MAP
 }
@@ -110,3 +114,43 @@ CRecordset* RecView::OnGetRecordset()
 /////////////////////////////////////////////////////////////////////////////
 // RecView message handlers
 
+void RecView::OnPrint(CDC* pDC, CPrintInfo* pInfo) {
+	int w = pDC->GetDeviceCaps(HORZRES);
+	int h = pDC->GetDeviceCaps(VERTRES);
+
+	CSize fontSize = pDC->GetTextExtent("ID");
+
+	int x = w / 10;
+	int y = h / 10;
+
+	pDC->TextOut(x, y, "id");
+	pDC->TextOut(x*2, y, "name");
+	pDC->TextOut(x*5, y, "manager");
+
+	y += fontSize.cy * 2;
+
+	pDC->MoveTo(x, y);
+	pDC->LineTo(x * 7, y);
+
+	Set rs;
+
+	rs.Open();
+
+	while (!rs.IsEOF()) {
+
+		CString id;
+		id.Format("%d", rs.m_id);
+
+		y += fontSize.cy;
+
+		pDC->TextOut(x, y, id);
+		pDC->TextOut(x * 2, y, rs.m_name);
+
+		if (rs.m_manager) {
+			pDC->TextOut(x * 5, y, 'x');
+		}
+
+		rs.MoveNext();
+	}
+
+}
