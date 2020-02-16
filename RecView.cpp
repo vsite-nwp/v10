71a2345a94780bb678b1b45402171abf51df1c98
Auro Mazzi <auro.mazzi@gmail.com>
2020-02-16 15:16:27 +0100

    - Debug Assertion Failed Error if GetTextExtent is used - No Unicode - If GetTextExtent is not used Print and Print Preview differ

--- RecView.cpp
+++ RecView.cpp
@@ -45,6 +45,9 @@ void RecView::DoDataExchange(CDataExchange* pDX)
 	CRecordView::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(RecView)
 	//}}AFX_DATA_MAP
+	DDX_FieldText(pDX, IDC_EDIT1, m_pSet->m_id, m_pSet);
+	DDX_FieldText(pDX, IDC_EDIT2, m_pSet->m_name, m_pSet);
+	DDX_FieldCheck(pDX, IDC_CHECK1, m_pSet->m_manager, m_pSet);
 }
 
 BOOL RecView::PreCreateWindow(CREATESTRUCT& cs)
@@ -76,6 +79,35 @@ void RecView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
 
 void RecView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
 {
+}
+
+void RecView::OnPrint(CDC* pDC, CPrintInfo* pInfo)
+{
+	const int height = pDC->GetDeviceCaps(VERTRES);
+	const int width = pDC->GetDeviceCaps(HORZRES);
+	CSize fontSize = pDC->GetTextExtent(_T("O"));
+	//fontSize.cy = 100;
+	int vspace = fontSize.cy;
+	pDC->TextOut(width * 0.1, vspace, _T("ID"));
+	pDC->TextOut(width * 0.3, vspace, _T("Ime"));
+	pDC->TextOut(width * 0.5, vspace, _T("Manager"));
+	vspace += fontSize.cy * 2;
+	pDC->MoveTo(width * 0.1, vspace);
+	pDC->LineTo(width * 0.6, vspace);
+	
+	Set rs;
+	rs.Open();
+	while (!rs.IsEOF()) {
+		CString id;
+		id.Format(_T("%d"), rs.m_id);
+		pDC->TextOut(width * 0.1, vspace += fontSize.cy, id);
+		pDC->TextOut(width * 0.3, vspace, _T(rs.m_name));
+		if (rs.m_manager)
+			pDC->TextOut(width * 0.5, vspace, "X");
+		vspace += fontSize.cy;
+		rs.MoveNext();
+	}
+	rs.Close();
 }
 
 /////////////////////////////////////////////////////////////////////////////
