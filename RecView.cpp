6168beda7500e3f0e26026a8b96aee8cb0d8a8dc
Miomir Lovric <miomir.oe@gmail.com>
2022-06-09 01:19:49 +0200

    V10 gotov

--- RecView.cpp
+++ RecView.cpp
@@ -45,6 +45,9 @@ void RecView::DoDataExchange(CDataExchange* pDX)
 	CRecordView::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(RecView)
 	//}}AFX_DATA_MAP
+	DDX_FieldText(pDX, IDC_EDIT1, m_pSet->m_id, m_pSet);
+	DDX_FieldText(pDX, IDC_EDIT2, m_pSet->m_name, m_pSet);
+	DDX_FieldText(pDX, IDC_CHECK1, m_pSet->m_manager, m_pSet);
 }
 
 BOOL RecView::PreCreateWindow(CREATESTRUCT& cs)
@@ -110,3 +113,36 @@ CRecordset* RecView::OnGetRecordset()
 /////////////////////////////////////////////////////////////////////////////
 // RecView message handlers
 
+
+
+void RecView::OnPrint(CDC* pDC, CPrintInfo* pInfo)
+{
+	// TODO: Add your specialized code here and/or call the base class
+
+	CRecordView::OnPrint(pDC, pInfo);
+	
+	CSize const redak = pDC->GetTextExtent("ABC");
+	int x = pDC->GetDeviceCaps(HORZRES);
+	int y = pDC->GetDeviceCaps(VERTRES);
+	x = x / 9;
+	y = y / redak.cy;
+	pDC->TextOutA(x, y, "  id");
+	pDC->TextOutA(x*4, y, "name");
+	pDC->TextOutA(x*7, y, "manager");
+	pDC->MoveTo(x, y + redak.cy);
+	pDC->LineTo(x * 8, y + redak.cy);
+
+	Set rs;
+	rs.Open();
+	while (!rs.IsEOF()) {
+		// draw current record
+		y += redak.cy;//enter
+		CString id;
+		id.Format("%d", rs.m_id); // tip long u string
+		pDC->TextOut(x, y, id);
+		pDC->TextOut(x * 4, y, rs.m_name);
+		if (rs.m_manager)
+			pDC->TextOut(x * 7, y, "X");
+		rs.MoveNext();
+	}
+}
