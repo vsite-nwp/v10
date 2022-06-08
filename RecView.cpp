560ce8a05092949c0110808bc4cffe3369f3e805
Kristijan <Kristijan@DESKTOP-G5S3BKQ>
2022-06-08 15:17:38 +0200

    Resolved

--- RecView.cpp
+++ RecView.cpp
@@ -45,6 +45,9 @@ void RecView::DoDataExchange(CDataExchange* pDX)
 	CRecordView::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(RecView)
 	//}}AFX_DATA_MAP
+	DDX_FieldCheck(pDX, IDC_CHECK1, m_pSet->m_manager, m_pSet);
+	DDX_FieldText(pDX, IDC_EDIT1, m_pSet->m_id, m_pSet);
+	DDX_FieldText(pDX, IDC_EDIT2, m_pSet->m_name, m_pSet);
 }
 
 BOOL RecView::PreCreateWindow(CREATESTRUCT& cs)
@@ -73,7 +76,41 @@ BOOL RecView::OnPreparePrinting(CPrintInfo* pInfo)
 void RecView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
 {
 }
+void RecView::OnPrint(CDC* pDC, CPrintInfo* printInfo)
+{
+	const int width = pDC->GetDeviceCaps(HORZRES);
+	const int height = pDC->GetDeviceCaps(VERTRES);
 
+	const CSize textExtent = pDC->GetTextExtent("A");
+
+	const int x = width / 10;
+	int y = textExtent.cy * 3;
+
+	pDC->TextOutA(x, y, "ID");
+	pDC->TextOutA(x * 2, y, "Name");
+	pDC->TextOutA(x * 4, y, "Manager");
+
+	pDC->MoveTo(x, y += textExtent.cy);
+	pDC->LineTo(x * 10, y);
+
+	Set rs;
+	rs.Open();
+
+	while (!rs.IsEOF())
+	{
+		CString id;
+		id.Format("%d", rs.m_id);
+		pDC->TextOut(x, y += textExtent.cy, id);
+		pDC->TextOut(x * 2, y, rs.m_name);
+
+		if (rs.m_manager)
+		{
+			pDC->TextOut(x * 4, y, "X");
+		}
+
+		rs.MoveNext();
+	}
+}
 void RecView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
 {
 }
