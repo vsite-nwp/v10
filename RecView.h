cc12360251a09833a1494f6b181a4b29090cf1cc
Kristian Bolarić <kristian.bolaric@gmail.com>
2024-06-03 18:37:53 +0200

    Vjezba 10

--- RecView.h
+++ RecView.h
@@ -27,16 +27,17 @@ public:
 public:
 	Doc* GetDocument();
 
-// Operations
+	// Operations
 public:
 
-// Overrides
-	// ClassWizard generated virtual function overrides
-	//{{AFX_VIRTUAL(RecView)
-	public:
+	// Overrides
+		// ClassWizard generated virtual function overrides
+		//{{AFX_VIRTUAL(RecView)
+public:
+	virtual void OnPrint(CDC* pDC, CPrintInfo* pInfo);
 	virtual CRecordset* OnGetRecordset();
 	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
-	protected:
+protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 	virtual void OnInitialUpdate(); // called first time after construct
 	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
@@ -54,7 +55,7 @@ public:
 
 protected:
 
-// Generated message map functions
+	// Generated message map functions
 protected:
 	//{{AFX_MSG(RecView)
 	//}}AFX_MSG
@@ -63,7 +64,9 @@ protected:
 
 #ifndef _DEBUG  // debug version in RecView.cpp
 inline Doc* RecView::GetDocument()
-   { return (Doc*)m_pDocument; }
+{
+	return (Doc*)m_pDocument;
+}
 #endif
 
 /////////////////////////////////////////////////////////////////////////////
